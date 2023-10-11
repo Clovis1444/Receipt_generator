2c87165d1c6483e430ea2ccba47656c5bac1722f
CumZone1488 <eu11111444@gmail.com>
2023-10-11 03:36:54 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 
 int main() {
+
   std::chrono::time_point currentTime{getCurrentTime()};
   std::chrono::year_month_day ymd{getYMD(currentTime)};
   const std::string time{getRandomHM()};
