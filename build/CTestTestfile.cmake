b233b3745d9b6ef2f522eecae3fc38be3ebe39fb
CumZone1488 <eu11111444@gmail.com>
2023-10-25 22:13:55 +0300

    Second

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Projects/С++/Receipt_generator
+# Build directory: E:/Projects/С++/Receipt_generator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
